73e73ab7fcfc1a7517a843b189968457424a6dc7
Alberto Domingues <94317971+albertodomingues81@users.noreply.github.com>
2023-02-28 15:33:21 +0000

    Primeiros passos na estrutura de um componente

--- react-dio/src/App.js
+++ react-dio/src/App.js
@@ -1,6 +1,13 @@
 const App = () => {
   return(
-  <h1>Hello world</h1>
+    <>
+  <h1>Minha primeira aplicação com React</h1>
+      <ul>
+        <a href="#" className="list-group-item list-group-item-action list-group-item-dark"></a>
+        <li>Item 2</li>
+        <li>Item 3</li>
+      </ul>
+      </>
     )
 }
  export default App;
