c1ad02c6752d97922ec8ea97bfe75bc1a0ef9c6a
Alberto Domingues <94317971+albertodomingues81@users.noreply.github.com>
2023-03-03 15:31:54 +0000

    Importando um componente

--- react-dio/src/App.js
+++ react-dio/src/App.js
@@ -1,11 +1,22 @@
+import Item from './componentes/item';
+
 const App = () => {
   return(
     <>
   <h1>Minha primeira aplicação com React</h1>
       <ul>
-        <a href="#" className="list-group-item list-group-item-action list-group-item-dark"></a>
-        <li>Item 2</li>
-        <li>Item 3</li>
+        <Item>
+          Item 1
+        </Item>
+        <Item>
+          Item 2
+        </Item>
+        <Item>
+          Item 3
+        </Item>
+        
+        
+       
       </ul>
       </>
     )
--- react-dio/src/componentes/item/index.js
+++ react-dio/src/componentes/item/index.js
@@ -1,8 +1,7 @@
 const Item = () => {
   return (
     <a href="#" className="list-group-item list-group-item-action list-group-item-dark">
-       Item 1
-    
+      {props.children}   
     </a>
    
   )
